e550b510ee18b4c58082928fcb978c4b45126321
Mamun <a.a.mamunbu@gmail.com>
2022-09-27 16:42:57 +0600

    updated dir

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from log_handler import LogHandler
 
 if __name__== '__main__':
     MongoDbClient.init_db()
-    LogHandler.LogHandler('C:/Users/aristo/Desktop/task', '').isfileOrDir()
+    LogHandler.LogHandler('C:/Users/aristo/Desktop/testbench/logs', '').isfileOrDir()
